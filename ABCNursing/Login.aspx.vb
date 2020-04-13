533fbbcc3ef50c4bc74eea779d975633ef44e3e4
Ryan <47826566+discotwig@users.noreply.github.com>
2020-04-13 18:23:04 -0400

    update login code, add proper sql con string

--- ABCNursing/Login.aspx.vb
+++ ABCNursing/Login.aspx.vb
@@ -20,7 +20,7 @@ Partial Class Login
         FormsAuthentication.Initialize()
         Dim userId As Integer = 0
         Dim roles As String = String.Empty
-        Dim con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDBFilename=|DataDirectory|\WWWDatabase.mdf;Integrated Security=True")
+        Dim con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True")
         Using cmd As New SqlCommand("Validate_User")
             cmd.CommandType = CommandType.StoredProcedure
             cmd.Parameters.AddWithValue("@Username", Login1.UserName)
@@ -41,8 +41,7 @@ Partial Class Login
                 Login1.FailureText = "Account has not been activated."
                 Exit Select
             Case Else
-                Dim ticket As New FormsAuthenticationTicket(1, Login1.UserName, DateTime.Now, DateTime.Now.AddMinutes(2880), Login1.RememberMeSet, roles,
-FormsAuthentication.FormsCookiePath)
+                Dim ticket As New FormsAuthenticationTicket(1, Login1.UserName, DateTime.Now, DateTime.Now.AddMinutes(2880), Login1.RememberMeSet, roles, FormsAuthentication.FormsCookiePath)
                 Dim hash As String = FormsAuthentication.Encrypt(ticket)
                 Dim cookie As New HttpCookie(FormsAuthentication.FormsCookieName, hash)
                 If ticket.IsPersistent Then
