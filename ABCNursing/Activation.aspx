b30a23bfee786dcb7699cb71fb8c79e8706bd99d
Ryan <47826566+discotwig@users.noreply.github.com>
2020-04-13 18:36:09 -0400

    add activation page, add generic activation code. Not complete

--- ABCNursing/Activation.aspx
+++ ABCNursing/Activation.aspx
@@ -3,7 +3,9 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>Activation</h1>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+    <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
 </asp:Content>
 
--- ABCNursing/Activation.aspx.vb
+++ ABCNursing/Activation.aspx.vb
@@ -1,5 +1,32 @@
-﻿
+﻿Imports System.Data
+Imports System.Configuration
+Imports System.Data.SqlClient
 Partial Class Activation
     Inherits System.Web.UI.Page
-
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not Me.IsPostBack Then
+            Dim constr As String =
+           ConfigurationManager.ConnectionStrings("WWWDataBase").ConnectionString
+            Dim activationCode As String = If(Not String.IsNullOrEmpty(Request.QueryString("ActivationCode")), Request.QueryString("ActivationCode"), Guid.Empty.ToString())
+            Using con As New SqlConnection(constr)
+                Using cmd As New SqlCommand("DELETE FROM UserActivation WHERE ActivationCode = @ActivationCode")
+                    Using sda As New SqlDataAdapter()
+                        cmd.CommandType = CommandType.Text
+                        cmd.Parameters.AddWithValue("@ActivationCode", activationCode)
+                        cmd.Connection = con
+                        con.Open()
+                        Dim rowsAffected As Integer = cmd.ExecuteNonQuery()
+                        con.Close()
+                        If rowsAffected = 1 Then
+                            lblMessage.Text = "Activation Has Been Successful!"
+                            lblLogIn.Text = <a href="/LogIn.aspx">Log In</a>
+                        Else
+                            lblMessage.Text = "Error, Invalid Activation Code."
+                            lblLogIn.Text = " "
+                        End If
+                    End Using
+                End Using
+            End Using
+        End If
+    End Sub
 End Class
