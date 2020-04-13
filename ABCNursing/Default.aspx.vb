c1133f0756ca0b7809ade25dd4623013b030c707
Ryan <47826566+discotwig@users.noreply.github.com>
2020-04-13 18:36:28 -0400

    add generic default code. No welcome label yet

--- ABCNursing/Default.aspx.vb
+++ ABCNursing/Default.aspx.vb
@@ -1,5 +1,13 @@
-﻿
-Partial Class _Default
+﻿Partial Class _Default
     Inherits System.Web.UI.Page
-
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load()
+        ' Find if user is logged in
+        If (Context.User.Identity.IsAuthenticated) Then
+            ' Finds user name and says Hi
+            lblWelcome.Text = "Hello " + Context.User.Identity.Name & "!"
+        Else
+            ' It is anonymous user, say hi to guest
+            lblWelcome.Text = "Hello Guest!"
+        End If
+    End Sub
 End Class
