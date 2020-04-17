7abbb83009f0ecd18d9f4a1ee03e882704b0ebb0
Ryan <47826566+discotwig@users.noreply.github.com>
2020-04-17 16:30:37 -0400

    Make master signup, login links

--- ABCNursing/ABCNursingMasterPage.master.vb
+++ ABCNursing/ABCNursingMasterPage.master.vb
@@ -4,14 +4,14 @@ Partial Class ABCNursingMasterPage
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         ' Find if user is logged in
         If (Context.User.Identity.IsAuthenticated) Then
-            lblUser.Text = <a href="/LogIn.aspx">Sign Out</a>
+            lblUser.Text = <a href="Login.aspx">Sign Out</a>
             lblSignUp.Text = " "
         Else
-            lblUser.Text = <a href="/Login.aspx"> Log In</a>
-            lblSignUp.Text = <a href="/Signup.aspx">Sign Up</a>
+            lblUser.Text = <a href="Login.aspx"> Log In</a>
+            lblSignUp.Text = <a href="Signup.aspx"> Sign Up</a>
         End If
         If (Context.User.IsInRole("Admin")) Then
-            lblAdmin.Text = <a href="/AdminPages/AdminUserProfile.aspx">Admin</a>
+            lblAdmin.Text = <a href="AdminPages/AdminUserProfile.aspx"> Admin</a>
         End If
     End Sub
 End Class
